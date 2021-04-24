ecc89a77f5595f28cfdbfe44750adbaed45d42d4
OrangeKir <fraiming_king@outlook.com>
2021-04-24 12:02:02 +0300

    TA-1

---
+++ TestApp1/Class1.cs
@@ -0,0 +1,12 @@
+﻿namespace TestApp1
+{
+    using System;
+
+    public class Class1
+    {
+        public static void Run()
+        {
+            Console.WriteLine("Сам иди");
+        }
+    }
+}
---
+++ TestApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace TestApp1124
+{
+    using System;
+    using TestApp1;
+
+    class Program12312412
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Пошел нахуй");
+            Class1.Run();
+        }
+    }
+}
